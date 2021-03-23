3f04d544fda979195fcde6e4eedb2e4916efaa29
MooseOnTheRocks <sam.foltz@hotmail.com>
2021-03-22 20:43:47 -0700

    A small step for rendering.

---
+++ src/Renderer.ts
@@ -0,0 +1,21 @@
+function main() {
+    const canvas: HTMLCanvasElement | null = document.querySelector("#gameCanvas");
+    if (!canvas) {
+        alert("No canvas!");
+        return;
+    }
+    console.log("Hi");
+    canvas.width = 800;
+    canvas.height = 600;
+    const gl: WebGLRenderingContext | null = canvas.getContext("webgl");
+
+    if (!gl) {
+        alert("Need webgl!");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+window.onload = main;
