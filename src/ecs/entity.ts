b0bc31ff49c67d0f20a62cac0d69e4d1f4702554
TristanGavin <gavin.tristan@gmail.com>
2021-03-23 21:11:22 -0700

    added functions to entity (get, add)

--- src/ecs/entity.ts
+++ src/ecs/entity.ts
@@ -1,3 +1,21 @@
 import {Component} from './component'
 
-export abstract class Entity {}
+export abstract class Entity {
+    protected _components: Component[] = [] // tracks components attached to this entity.
+
+    //get full list of components
+    public get Components(): Component[] {
+        return this._components
+    }
+
+    //need to be able to add components
+    public addComponent(component: Component): void {
+        this._components.push(component)
+        component.Entity = this  //attach entity to component on component side. *** 
+    }
+
+    // need to be able to get specific component
+    public getComponent(component: Component): Component {
+        // search for component in array
+    }
+}
